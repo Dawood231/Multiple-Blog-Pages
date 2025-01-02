3e0b531f07b8cdfa35c9efb55c8c656822ccc6bd
Muhammad Dawood Siddiqui <164146729+Dawood231@users.noreply.github.com>
2025-01-02 12:40:25 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,29 @@ import Link from 'next/link';
 import './globals.css';
 import blog1 from './images/blog1.jpg';
 import blog2 from './images/blog2.jpg';
-import blog3 from  './images/blog3.jpg';
-import blog4 from  './images/blog4.jpg';
-
+import blog3 from './images/blog3.jpg';
+import blog4 from './images/blog4.jpg';
 // images
 import Image from 'next/image';
+import Header from './components/header';
+import Footer from './components/footer';
 
 const posts = [
   { id: 1, image: blog1, title: 'Artificial intelligence', content: 'Artificial intelligence (AI) is the science and engineering of creating intelligent machines that can perform tasks that typically require human intelligence.' },
-  { id: 2, image: blog2, title: 'Imapct Of Technology', content: 'Technology has many impacts on society, including positive and negative effects.' },
-  { id: 3, image: blog3, title: 'Agentic AI Future 2025', content: 'Agentic AI is a type of AI thats all about autonomy.' },
-  { id: 4, image: blog4, title: 'Most Valuable Programming Language', content: 'The "most valuable" programming language can vary depending on context, such as industry, job market, or the specific use  .' },
+  { id: 2, image: blog2, title: 'Impact Of Technology', content: 'The impact of technology on society is profound and multifaceted, touching nearly every aspect of our lives..' },
+  { id: 3, image: blog3, title: 'Future 2025 Agentic AI', content: 'Agentic AI refers to artificial intelligence systems that are capable of performing tasks autonomously, acting as agents that make decisions and take actions in the real world without constant human intervention or supervision. .' },
+  { id: 4, image: blog4, title: 'Programming Languages', content: 'When discussing valuable programming languages (often abbreviated as "valuable programming languages" or "top programming languages"), the answer can vary depending on factors like industry trends, job opportunities etc .' },
 ];
 
 export default function Home() {
   return (
     <div className='blog-page'>
-      <header>
-        <div className="logo">Latest Blog Website</div>
-        <nav>
-          <Link href={'/'} className='link' >Home</Link>
-          <Link href={'/'} className='link' >Latest post</Link>
-          <Link href={'/'} className='link' >About</Link>
-          <Link href={'/'} className='link' >Contact</Link>
-        </nav>
-      </header>
+      <Header/>
 
       <div className='blog-cont'>
         {posts.map((post) => (
           <div key={post.id} className='blog-card'>
-            <Image src={post.image} alt='blog-1' className='blog-img'></Image>
+            <Image src={post.image} alt={post.title} className='blog-img'></Image>
             <h1>{post.title}</h1>
             <p>{post.content}</p>
             <Link href={`/posts/${post.id}`}>
@@ -40,6 +33,8 @@ export default function Home() {
           </div>
         ))}
       </div>
+
+      <Footer/>
     </div>
   );
 }
