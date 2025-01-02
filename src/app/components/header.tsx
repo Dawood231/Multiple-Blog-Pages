ef475e98e40094847d86e32d53c1e933cc293422
Muhammad Dawood Siddiqui <164146729+Dawood231@users.noreply.github.com>
2025-01-02 12:42:07 +0500

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,1 +1,16 @@
+import Link from 'next/link';
+import '@/app/globals.css'
 
+export default function Header() {
+  return (
+    <header>
+        <div className="logo">Latest Blog Website</div>
+        <nav>
+          <Link href={'/'} className='link' >Home</Link>
+          <Link href={'/'} className='link' >Latest post</Link>
+          <Link href={'/'} className='link' >About</Link>
+          <Link href={'/'} className='link' >Contact</Link>
+        </nav>
+      </header>
+  )
+}
